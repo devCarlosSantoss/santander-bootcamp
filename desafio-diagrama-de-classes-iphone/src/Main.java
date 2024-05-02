f304a631f8ee04e95c9ae3a91fc5862f45de72ce
devCarlosSantoss <carlosvinicius357890@gmail.com>
2024-05-02 00:25:36 -0300

    resolucao desafio

---
+++ desafio-diagrama-de-classes-iphone/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        iPhone meuiPhone = new iPhone();
+        meuiPhone.tocar();
+        meuiPhone.ligar("123456789");
+        meuiPhone.exibirPagina("http://www.example.com");
+    }
+}
---
+++ desafio-diagrama-de-classes-iphone/src/iPhone.java
@@ -0,0 +1,46 @@
+public class iPhone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando a música: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Chamada atendida.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Correio de voz iniciado.");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo a página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        System.out.println("Nova aba adicionada para a página: " + url);
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+}
